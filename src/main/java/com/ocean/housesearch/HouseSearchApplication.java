98a149e7a35f4e4151fa53329af8f584bdd92637
niuqh@bjnangle.com <56342786n>
2019-05-05 17:26:08 +0800

    Initial commit

---
+++ src/main/java/com/ocean/housesearch/HouseSearchApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.housesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseSearchApplication.class, args);
+    }
+
+}
